0081d1556d11ac9299d64cf53e0280d524b9a6a0
Dinesh Angadipeta <xxdinesh.axx@gmail.com>
2024-04-15 21:08:52 -0500

    Some minor changes to change typos

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,13 +10,13 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="images/img-10.jpg"
+              src="/images/img-10.jpg"
               text="Fight back against Cancer"
               label="Medical"
               path="/services"
             ></CardItem>
             <CardItem
-              src="images/img-3.jpg"
+              src="/images/img-3.jpg"
               text="Help combat against illegal Whaling"
               label="Emergency Relief"
               path="/services"
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="images/img-12.jpg"
+              src="/images/img-12.jpg"
               text="Rebuild broken hearts, Help aid Victims of Earthquakes"
               label="Emergency Relief"
               path="/services"
             />
             <CardItem
-              src="images/img-13.jpg"
+              src="/images/img-13.jpg"
               text="Help rebuild a beloved run down Trail"
               label="Community"
               path="/services"
             />
             <CardItem
-              src="images/img-11.jpg"
+              src="/images/img-11.jpg"
               text="Ease the scars of war, help Veterans with PTSD"
               label="Medical"
               path="/services"
