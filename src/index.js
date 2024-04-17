89dcfa0cfc296739792dac69cdabc8a9b24c30c5
Julia Olores <oloresjulia@gmail.com>
2024-04-17 10:31:11 -0500

    install firebase and config

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBsKMrz9sACS0Tx8CG2mNlMtDPOxHT0HPM",
+    authDomain: "missionmatch-3fb9a.firebaseapp.com",
+    projectId: "missionmatch-3fb9a",
+    storageBucket: "missionmatch-3fb9a.appspot.com",
+    messagingSenderId: "469502442024",
+    appId: "1:469502442024:web:25c4305fb68af05529131d",
+    measurementId: "G-2KYPEMD00C"
+});
