b4b3f36dfdb7e220d12e8016ebab99de2c33fd2b
Dinesh Angadipeta <xxdinesh.axx@gmail.com>
2024-05-02 20:12:30 -0500

    [FRONT END] Added donation goal input field to posting application

--- src/components/pages/Application.jsx
+++ src/components/pages/Application.jsx
@@ -1,17 +1,22 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 function Application() {
   const [formData, setFormData] = useState({
     title: "",
     description: "",
+    donationGoal: "",
   });
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    // Validate input to allow only positive numbers
+    const { name, value } = e.target;
+    const newValue = value.replace(/\D/g, ""); // Remove non-digit characters
+    setFormData({ ...formData, [name]: newValue });
   };
 
   const handleSignUpSubmit = (e) => {
     e.preventDefault();
+    // Add your form submission logic here
   };
 
   useEffect(() => {
@@ -42,6 +47,16 @@ function Application() {
             onChange={handleChange}
             required
           />
+          <input
+            className="signup-input"
+            type="text"
+            placeholder="Donation Goal"
+            name="donationGoal"
+            value={formData.donationGoal}
+            onChange={handleChange}
+            required
+          />
+
           <button type="submit" className="signup-btn">
             Post Application
           </button>
