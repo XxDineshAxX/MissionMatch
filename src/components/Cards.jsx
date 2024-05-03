56e5d156338cab2213152172aa73915e1d03f077
Avinash Nair <avinair108@gmail.com>
2024-05-02 22:58:21 -0500

    grants almost done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,68 +1,74 @@
 import CardItem from "./CardItem";
 import "./Cards.css";
 import { SigninContext } from "../contexts/SigninContext";
-import { useState, useEffect, useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  updateDoc,
+  setDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
+import { db } from '../index'
 
 function Cards() {
   const { currentUser } = useContext(SigninContext);
+  const [nonProfits, setNonProfits] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchNonProfits = async () => {
+      try {
+        const q = query(collection(db, "users"),
+          where("userType", "==", "non-profit")); 
+        const querySnapshot = await getDocs(q);
+        const nonProfitList = [];
+        for (const docRef of querySnapshot.docs) {
+          const donorData = docRef.data();
+          const donorGrantsRef = doc(db, "userGrants", donorData.uid);
+          const donorGrantsDoc = await getDoc(donorGrantsRef);
+          if (donorGrantsDoc.exists() && donorGrantsDoc != null) {
+            console.log(donorGrantsDoc.data().donationType);
+            nonProfitList.push({ ...donorData, grants: donorGrantsDoc.data() });
+          } 
+        }
+        // in case of expansion
+        // const shuffledNonProfits = nonProfitList.sort(() => Math.random() - 0.5);
+        // const randomNonProfits = shuffledNonProfits.slice(0, 10);
+        setNonProfits(nonProfitList);
+        console.log(nonProfitList[0])
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchNonProfits()
+  }, []); 
+
   return (
     <div className="cards">
       <h1>Discover Missions inspired by many like you</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
+
           <ul className="cards__items">
-            <CardItem
-              src="./images/img-10.jpg"
-              text="Fight back against Cancer"
-              label="Medical"
-              path={
-                currentUser != null
-                  ? "/MissionMatch/grant"
-                  : "/MissionMatch/sign-up"
-              }
-            ></CardItem>
-            <CardItem
-              src="./images/img-3.jpg"
-              text="Help combat against illegal Whaling"
-              label="Emergency Relief"
-              path={
-                currentUser != null
-                  ? "/MissionMatch/grant"
-                  : "/MissionMatch/sign-up"
-              }
-            ></CardItem>
-          </ul>
-          <ul className="cards__items">
-            <CardItem
-              src="./images/img-12.jpg"
-              text="Rebuild broken hearts, Help aid Victims of Earthquakes"
-              label="Emergency Relief"
-              path={
-                currentUser != null
-                  ? "/MissionMatch/grant"
-                  : "/MissionMatch/sign-up"
-              }
-            />
-            <CardItem
-              src="./images/img-13.jpg"
-              text="Help rebuild a beloved run down Trail"
-              label="Community"
-              path={
-                currentUser != null
-                  ? "/MissionMatch/grant"
-                  : "/MissionMatch/sign-up"
-              }
-            />
-            <CardItem
-              src="./images/img-11.jpg"
-              text="Ease the scars of war, help Veterans with PTSD"
-              label="Medical"
-              path={
-                currentUser != null
-                  ? "/MissionMatch/grant"
-                  : "/MissionMatch/sign-up"
-              }
-            />
+          {error && <div>Error: {error}</div>}
+          {nonProfits.map(nonProfit => (
+          <CardItem
+          id={nonProfit.id}
+          src="./images/img-12.jpg"
+          text={nonProfit.grants.description}
+          label={nonProfit.grants.title}
+          path={
+            currentUser != null
+              ? "/MissionMatch/grant"
+              : "/MissionMatch/sign-up"
+          }
+        />
+        ))}
           </ul>
         </div>
       </div>
